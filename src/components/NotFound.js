ba073ac766193ecb803d218eea5916b398d819f2
David <david.graf28@gmail.com>
2020-06-05 22:04:39 +0200

    Deploy the App on GitHub

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
 
 
 const NotFound = () => {
+    let history = useHistory();
+
     return (
         <div className='container center'>
             <h1>Erreur page introuvable</h1>
+            {history.push('/')}
         </div>
     )
 }
