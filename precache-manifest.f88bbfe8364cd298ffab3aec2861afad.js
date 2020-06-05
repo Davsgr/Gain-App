7507ac777d8cfbb33eed63476baa62eac5dd5303
David <david.graf28@gmail.com>
2020-06-05 21:24:53 +0200

    Updates

---
+++ precache-manifest.f88bbfe8364cd298ffab3aec2861afad.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745f98fdd944e815ed044a8df155cab7",
+    "url": "/Gain-App/index.html"
+  },
+  {
+    "revision": "0895d8ecbd9bcc9d34d6",
+    "url": "/Gain-App/static/css/main.758cc774.chunk.css"
+  },
+  {
+    "revision": "5c46697cff6615c5b54f",
+    "url": "/Gain-App/static/js/2.7cde521d.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/Gain-App/static/js/2.7cde521d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0895d8ecbd9bcc9d34d6",
+    "url": "/Gain-App/static/js/main.894e0c87.chunk.js"
+  },
+  {
+    "revision": "bae13a8b7769a2e89c83",
+    "url": "/Gain-App/static/js/runtime-main.593a365a.js"
+  },
+  {
+    "revision": "f1ef5cee298faaa1c3307254be0333a4",
+    "url": "/Gain-App/static/media/A1.f1ef5cee.png"
+  },
+  {
+    "revision": "34ecf0b64b2a121401f7d84937164cd7",
+    "url": "/Gain-App/static/media/A2.34ecf0b6.png"
+  },
+  {
+    "revision": "2d327666dfa716a33096e203545cfa95",
+    "url": "/Gain-App/static/media/A3.2d327666.png"
+  },
+  {
+    "revision": "242688512d52d2bb1a691a0dafc3e301",
+    "url": "/Gain-App/static/media/A4.24268851.png"
+  },
+  {
+    "revision": "f0f1be94981c64785b01bf7d60b99392",
+    "url": "/Gain-App/static/media/A5.f0f1be94.png"
+  },
+  {
+    "revision": "d94c1f66e60756a9a47f4c6560cb402f",
+    "url": "/Gain-App/static/media/A6.d94c1f66.png"
+  },
+  {
+    "revision": "33def4b211c917538078ab11288472dd",
+    "url": "/Gain-App/static/media/A7.33def4b2.png"
+  },
+  {
+    "revision": "e991d7563e1013f52e92734b053553ab",
+    "url": "/Gain-App/static/media/G1.e991d756.png"
+  },
+  {
+    "revision": "50baa251ac00fffd22cf770eaba95d7d",
+    "url": "/Gain-App/static/media/G2.50baa251.png"
+  },
+  {
+    "revision": "e1e4740bf17febe5c2f5df450a2ef35b",
+    "url": "/Gain-App/static/media/G3.e1e4740b.png"
+  },
+  {
+    "revision": "0e1b149846acb0a852ae0f5710a05bc0",
+    "url": "/Gain-App/static/media/G4.0e1b1498.png"
+  },
+  {
+    "revision": "e3ddd192d3082943d72f5e5dfa2c5cd4",
+    "url": "/Gain-App/static/media/G5.e3ddd192.png"
+  },
+  {
+    "revision": "c9fc33c932943b9383d6d290cc86b091",
+    "url": "/Gain-App/static/media/G6.c9fc33c9.png"
+  },
+  {
+    "revision": "f9df39dd672beca15c75a88b922feace",
+    "url": "/Gain-App/static/media/J1.f9df39dd.png"
+  },
+  {
+    "revision": "144997fb144034ca2741dab6593cbdae",
+    "url": "/Gain-App/static/media/J2.144997fb.png"
+  },
+  {
+    "revision": "1bc136810208c9a98fe49ec9b5c1a47f",
+    "url": "/Gain-App/static/media/J3.1bc13681.png"
+  },
+  {
+    "revision": "7f1eb2cc497c14ba49405da86f760858",
+    "url": "/Gain-App/static/media/J5.7f1eb2cc.png"
+  },
+  {
+    "revision": "d57b04f04550182a34d8865118a04ea2",
+    "url": "/Gain-App/static/media/L1.d57b04f0.png"
+  },
+  {
+    "revision": "76624176935f64d9c7e66716d70fc804",
+    "url": "/Gain-App/static/media/L2.76624176.png"
+  },
+  {
+    "revision": "a901d9c199a6948ad65012eff6be3b20",
+    "url": "/Gain-App/static/media/L3.a901d9c1.png"
+  },
+  {
+    "revision": "4bc3cd24030dcfa6a9a753c063d269ef",
+    "url": "/Gain-App/static/media/L4.4bc3cd24.png"
+  },
+  {
+    "revision": "d932c60652acf60924987457c32a3c44",
+    "url": "/Gain-App/static/media/beep.d932c606.mp3"
+  },
+  {
+    "revision": "ccdfa6b4afcfa41a3432dc90b1ff2a00",
+    "url": "/Gain-App/static/media/finish.ccdfa6b4.mp3"
+  },
+  {
+    "revision": "fa7c4d17284aa0a9d6c1358f9f0eafd3",
+    "url": "/Gain-App/static/media/pompe.fa7c4d17.png"
+  },
+  {
+    "revision": "604d5da2bd2e2a9bdeef5aca69698305",
+    "url": "/Gain-App/static/media/zero.604d5da2.mp3"
+  }
+]);
